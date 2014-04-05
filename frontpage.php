e8aed648e1ba8c3f1e7d3c8c1ff932eb6e890c8c
ft- <nomail>
2014-04-05 21:54:11 +0200

    added getNumberOfRegions functions

--- frontpage.php
+++ frontpage.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once("lib/services.php");
+require_once("lib/types/RegionInfo.php");
 $serverParams = getService("ServerParam");
-
+$gridService = getService("Grid");
 $gridname = $serverParams->getParam("gridname", "phpGridServer");
+$numregions = $gridService->getNumberOfRegionsFlags(null, RegionFlags::RegionOnline);
 ?>
 <html>
 <head>
@@ -11,6 +13,7 @@ $gridname = $serverParams->getParam("gridname", "phpGridServer");
 <link rel="stylesheet" type="text/css" href="/css/main.css"/>
 </head>
 <body>
-<center><h1><?php echo $gridname ?></h1></center>
+<center><h1><?php echo $gridname ?></h1></center><br/>
+<b>Regions online: <?php echo $numregions ?></b>
 </body>
 </html>
