9f69abbc53b9e4d51fb65caaf8d84957675af276
ft- <nomail>
2014-04-16 20:52:49 +0200

    added fromBytes to UUID

--- lib/types/UUID.php
+++ lib/types/UUID.php
@@ -20,7 +20,7 @@ class UUID
 		}
 		$this->ID = $str;
 	}
-	
+
 	public static function CheckWithException($str)
 	{
 		if(!preg_match("/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/", $str))
@@ -28,17 +28,17 @@ class UUID
 			throw new InvalidUUIDException("$str is not a valid UUID\n".print_r(debug_backtrace(), true));
 		}
 	}
-	
+
 	public function __isset(string $name)
 	{
 		return($name == "ID");
 	}
-	
+
 	public function __set($name, $value)
 	{
 		if($name == "ID")
 		{
-			if(UUID::IsUUID($value))
+			if(UUID::IsUUID("$value"))
 			{
 				$this->ID = "$value";
 			}
@@ -56,7 +56,7 @@ class UUID
 		    print_r(debug_backtrace(), true),
 		    E_USER_NOTICE);
 	}
-	
+
 	public function __get($name)
 	{
 		if($name == "ID")
@@ -66,43 +66,64 @@ class UUID
 		trigger_error(
 		    "Undefined property $name via __get()\n" .print_r(debug_backtrace(), true),
 		    E_USER_NOTICE);
-		return null;	
+		return null;
 	}
-	
+
 	public static function IsUUID($str)
 	{
 		return preg_match("/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/", $str);
 	}
-	
+
 	public function __toString()
 	{
 		return "".$this->ID."";
 	}
-	
+
 	public static function ZERO()
 	{
 		return new UUID();
 	}
-	
+
 	public static function Random()
 	{
 		return new UUID(sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
 			// 32 bits for "time_low"
 			mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-	 
+
 			// 16 bits for "time_mid"
 			mt_rand(0, 0xffff),
-	 
+
 			// 16 bits for "time_hi_and_version",
 			// four most significant bits holds version number 4
 			mt_rand(0, 0x0fff) | 0x4000,
-	 
+
 			// 16 bits, 8 bits for "clk_seq_hi_res",
 			// 8 bits for "clk_seq_low",
 			// two most significant bits holds zero and one for variant DCE1.1
 			mt_rand(0, 0x3fff) | 0x8000,
-	 
+
 			// 48 bits for "node"
 			mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)));
+	}
+
+	public static function fromBytes($input)
+	{
+		return new UUID(sprintf('%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x',
+				ord(substr($input,  0, 1)),
+				ord(substr($input,  1, 1)),
+				ord(substr($input,  2, 1)),
+				ord(substr($input,  3, 1)),
+				ord(substr($input,  4, 1)),
+				ord(substr($input,  5, 1)),
+				ord(substr($input,  6, 1)),
+				ord(substr($input,  7, 1)),
+				ord(substr($input,  8, 1)),
+				ord(substr($input,  9, 1)),
+				ord(substr($input, 10, 1)),
+				ord(substr($input, 11, 1)),
+				ord(substr($input, 12, 1)),
+				ord(substr($input, 13, 1)),
+				ord(substr($input, 14, 1)),
+				ord(substr($input, 15, 1))));
 	}
 };
