5d9bd0182a4d080831854487fa75ba8c0c1d8321
ft- <nomail>
2014-07-12 14:15:51 +0200

    added HG forwarding of local IM

---
+++ xmlrpc/xmlrpc_locate_user.php
@@ -0,0 +1,57 @@
+<?php
+/******************************************************************************
+ * phpGridServer
+ *
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ * Version 2.1, February 1999
+ *
+ */
+
+require_once("lib/services.php");
+require_once("lib/types/RegionInfo.php");
+require_once("lib/types/ServerDataURI.php");
+require_once("lib/helpers/capabilityPathes.php");
+require_once("lib/helpers/hgSession.php");
+
+$presenceService = getService("Presence");
+$hgTravelingDataService = getService("HGTravelingData");
+
+if(count($_RPC_REQUEST->Params)!=1)
+{
+	return new RPCFaultResponse(4, "Missing struct parameter");
+}
+
+$structParam = $_RPC_REQUEST->Params[0];
+
+$rpcStruct = new RPCStruct();
+
+if(isset($structParam->userID))
+{
+	$homeGrid = ServerDataURI::getHome();
+	try
+	{
+		$presence = $presenceService->getAgentByUUID($structParam->userID);
+		$rpcStruct->URL = $homeGrid->HomeURI;
+	}
+	catch(Exception $e)
+	{
+		try
+		{
+			$hgTravelingData = $hgTravelingDataService->getHGTravelingDataByAgentUUIDAndNotHomeURI($structParam->userID, $homeGrid->GridExternalName);
+			$rpcStruct->URL = $hgTravelingData->GridExternalName;
+		}
+		catch(Exception $e)
+		{
+			$rpcStruct->result = "Unable to locate user";
+		}
+	}
+}
+else
+{
+	$rpcStruct->result = "Unable to locate user";
+}
+
+$response = new RPCSuccessResponse();
+$response->InvokeID = $_RPC_REQUEST->InvokeID;
+$response->Params[] = $rpcStruct;
+return $response;
