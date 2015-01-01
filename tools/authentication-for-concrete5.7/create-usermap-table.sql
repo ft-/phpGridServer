152435dd8e03ed7a78db26cb3dc20d4987a8da0a
ft <ft@noemail>
2015-01-01 13:08:02 +0100

    corrected sql script for linux hosted mysql

--- tools/authentication-for-concrete5.7/create-usermap-table.sql
+++ tools/authentication-for-concrete5.7/create-usermap-table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `phpgridserverusermap` (
+CREATE TABLE `phpGridServerUserMap` (
   `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `binding` char(36) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
-INSERT INTO `authenticationtypes` VALUES (6,'phpgridserver','phpGridServer',0,0,0);
+INSERT INTO `AuthenticationTypes` VALUES (6,'phpgridserver','phpGridServer',0,0,0);
