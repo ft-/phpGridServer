c091615e760f3ea9bfde3d3f1f27f3c0ecba41a9
ft <ft@noemail>
2015-01-04 10:44:07 +0100

    added initialization of Inventory to inventory user page

--- user/pages/inventory.php
+++ user/pages/inventory.php
@@ -1,1 +1,4 @@
-<iframe id="inventory" name="inventory" style="position: relative; width: 100%; height: 100%; border-style: none;" src="/user/inventoryview.php"></iframe>
+<?php
+$inventoryService = getService("Inventory");
+$inventoryService->verifyInventory($_SESSION["principalid"]);
+?><iframe id="inventory" name="inventory" style="position: relative; width: 100%; height: 100%; border-style: none;" src="/user/inventoryview.php"></iframe>
